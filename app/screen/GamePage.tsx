2e084476604f1866b146704671503d13f98a9f01
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-17 16:02:53 +0530

    Gampage ui designed

--- app/screen/GamePage.tsx
+++ app/screen/GamePage.tsx
@@ -1,14 +1,87 @@
-import { Text, View } from "react-native";
+import NumberContainer from "@/components/NumberContainer";
+import PrimaryButton from "@/components/PrimaryButton";
+import Title from "@/components/Title";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
 
-export default function GamePage({ pickNumber }: { pickNumber: number }) {
+function generateRandomBetween(min: number, max: number, exclude: number) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+export default function GamePage({
+  pickNumber,
+  isGameOver,
+}: {
+  pickNumber: number | null;
+  isGameOver: () => void;
+}) {
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    pickNumber ?? 0,
+  );
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === pickNumber) {
+      isGameOver();
+    }
+  }, [currentGuess, pickNumber]);
+
+  function nextGuessHandler(direction: "lower" | "greater") {
+    if (
+      (direction === "lower" && currentGuess < (pickNumber ?? 0)) ||
+      (direction === "greater" && currentGuess > (pickNumber ?? 0))
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess,
+    );
+    setCurrentGuess(newRndNumber);
+  }
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 24 }}>Opponent's Guess</Text>
-      {/* Guess */}
+    <View style={style.container}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
-        <Text>Higher or Lower?</Text>+ -
+        <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => nextGuessHandler("greater")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       <View>{/* LOG Rounds */}</View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
