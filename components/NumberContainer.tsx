c6705490cf019c9aa0a2e4e8bfcb249ea5f36f50
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-18 11:59:06 +0530

    number container added

---
+++ components/NumberContainer.tsx
@@ -0,0 +1,31 @@
+import Colors from "@/constants/colors";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NumberContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    fontFamily: "open-sans-bold",
+  },
+});
