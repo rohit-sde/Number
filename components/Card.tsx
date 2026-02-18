b3bec61d564d9625cffb3f35d598f1efdb7b3503
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-18 11:57:52 +0530

    card component added

---
+++ components/Card.tsx
@@ -0,0 +1,23 @@
+import Colors from "@/constants/colors";
+import { StyleSheet, View } from "react-native";
+
+export default function Card({ children }: { children: React.ReactNode }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 36,
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: Colors.primary800,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
