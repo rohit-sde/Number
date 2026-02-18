7359e846232c622d444272d4c13f2d62fd48acd3
Rohit Singh <rohitsinghpb4@gmail.com>
2026-02-18 11:56:00 +0530

    navigated user accordingly game

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,66 @@
+import Colors from "@/constants/colors";
+import { useFonts } from "expo-font";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
-import { ImageBackground, StatusBar, StyleSheet } from "react-native";
+import { ImageBackground, StatusBar, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import GameOverPage from "./screen/GameOverPage";
 import GamePage from "./screen/GamePage";
 import StartGamePage from "./screen/StartGamePage";
 
 export default function RootLayout() {
   const [enteredNumber, setEnteredNumber] = useState<number | null>(null);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const [fontsLoaded] = useFonts({
+    "open-sans": require("../assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("../assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    // return <AppLoading />;
+    return <View></View>;
+  }
 
   function pickedNumberHandler(pickedNumber: number) {
     setEnteredNumber(pickedNumber);
   }
 
+  function gameOverHandler(numberOfRounds: number) {
+    setIsGameOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  function startNewGameHandler() {
+    setEnteredNumber(null);
+    setIsGameOver(false);
+  }
   let screen = <StartGamePage onPickNumber={pickedNumberHandler} />;
 
-  if (enteredNumber) {
-    screen = <GamePage pickNumber={enteredNumber} />;
+  if (enteredNumber && !isGameOver) {
+    screen = (
+      <GamePage pickNumber={enteredNumber} isGameOver={gameOverHandler} />
+    );
+  } else if (isGameOver) {
+    screen = (
+      <GameOverPage
+        roundsNumber={guessRounds}
+        userNumber={enteredNumber ?? 0}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
   }
   return (
     <LinearGradient
-      colors={["#4e0329", "#ddb52f"]}
+      colors={[Colors.primary800, Colors.accent500]}
       style={styles.rootContainer}
     >
       <ImageBackground
         source={require("../assets/images/background.png")}
         resizeMode="cover"
         style={styles.rootContainer}
-        imageStyle={{ opacity: 0.3 }}
+        imageStyle={{ opacity: 0.2 }}
       >
         <SafeAreaView style={styles.rootContainer}>{screen}</SafeAreaView>
       </ImageBackground>
